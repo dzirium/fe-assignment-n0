017a7e9537f2e05cf600911524b465c8a7032237
lokmane.ghedied <lokmane.ghedied@gmail.com>
2023-07-17 02:54:32 +0100

    TopBar done

--- src/Layouts/TopBar.jsx
+++ src/Layouts/TopBar.jsx
@@ -1,8 +1,30 @@
-import { Outlet } from "react-router-dom"
+//react-router-dom
+import { Outlet , NavLink } from "react-router-dom"
+//constants
+import { navLinks } from '../constants'
+//assets
+import logo from '../assets/logo.jpg'
+
 export const TopBar = () => {
   return (
     <>
-      <div>TopBar</div>
+      <header className="w-full bg-[#E5FFF3] flex justify-between items-center px-6">
+        {/* LOGO */}
+        <img className="h-[60px]" src={logo} alt="" />
+        
+        {/* LINKS */}
+        <div className="space-x-8">
+          {navLinks.map((link)=>(
+            <NavLink 
+                className={({ isActive }) =>
+                  isActive ? "font-semibold text-[48px]" : "text-[48px]"
+                }       
+              key={link.id} 
+              to={link.content}>{link.title}</NavLink>
+          ))}
+        </div>
+      </header>
+      
       <Outlet />
     </>
   )
---
+++ src/constants.js
@@ -0,0 +1,17 @@
+export const navLinks = [
+    {
+        id : 1,
+        title: 'Main',
+        content : '/'
+    },
+    {
+        id : 2,
+        title: 'About',
+        content : 'about'
+    },
+    {
+        id : 3,
+        title: 'Contact',
+        content : 'contact'
+    },
+]

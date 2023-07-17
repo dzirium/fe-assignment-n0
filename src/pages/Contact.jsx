9267f27f35fec922729105aa19b38b10be712bc3
lokmane.ghedied <lokmane.ghedied@gmail.com>
2023-07-17 04:08:50 +0100

    contact page Done

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,36 @@
+//assets
+import image from '../assets/image.png'
+//styles
+import {styles} from '../style'
 
 export const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+    return (
+      <div className="h-full w-full flex flex-col items-center justify-around">
+          
+          {/* TITLE */}
+          <h1 className={styles.title}>Contact Us</h1>
+          
+          {/* CONTENT */}
+          <section className='flex items-center justify-around w-full'>
+            <img className='w-[40%]' src={image} alt="" />
+
+            {/* ADDRESS */}
+            <span className='text-center'>
+              <p className={styles.subTitle}>Reach out at:</p>
+              <p className={styles.largeText}>42 Joe Mama st. - Algiers <br/> +213 550 88 48 15 <br/> scenthaven@outlook.dz</p>
+            </span>
+            {/* INPUTS */}
+            <span className='space-y-3'>
+              <p className={styles.subTitle}>Or send us a message:</p>
+              <p className={styles.largeText}>Name :</p>
+              <input type="text" className={styles.input} />
+              <p className={styles.largeText}>Message : </p>
+              <textarea name="message" className={`${styles.input} py-8`}></textarea>
+            </span>
+
+           
+          </section>
+      </div>
+    )
+  }
+  
--- src/style.js
+++ src/style.js
@@ -1,6 +1,8 @@
 export const styles = {
     title : 'text-6xl',
-    text : 'text-lg',
-    subTitle : 'text-xl'
+    subTitle : 'text-4xl ',
+    largeText : 'text-2xl ',
+    text : 'text-xl ',
+    input : 'border border-black w-full py-1'
 
 }

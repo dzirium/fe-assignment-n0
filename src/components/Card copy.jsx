56ab16899091b78777e6c5aad444d1f219bbe537
MaladAhmed <ahmed.benlahrech.c.s@gmail.com>
2023-07-18 04:27:53 +0100

    Added dynamic rendering based on fetched data

--- src/components/Card copy.jsx
+++ src/components/Card copy.jsx
@@ -1,33 +1,36 @@
 import React from "react";
 
-export default function Card(props
-
+export default function Card(
+  perfumeId,
+  perfumeBrand,
+  perfumeTitle,
+  perfumeDiscount,
+  perfumeRating,
+  perfumePrice,
+  perfumeImg
 ) {
-  // console.log('this is from Card:')
-  // console.log(props.perfumeImg  )
-  
   return (
     <div className="m-2 flex w-full max-w-xs flex-col rounded-lg border border-gray-100 bg-white shadow-md  ">
       <div className="relative mx-3 mt-3 flex h-40 overflow-hidden rounded-xl">
         {" "}
         <img
           className="object-contain"
-          src={props.perfumeImg}
+          src="https://i.dummyjson.com/data/products/11/1.jpg"
           alt="product image"
         />
         <span className="absolute top-0 left-0 m-2 rounded-full bg-black px-2 text-center text-sm font-medium text-white">
-          {props.perfumeDiscount}%
+          39% OFF
         </span>
       </div>
       <div className="mt-4 px-5 pb-5">
         <div className="">
           <h5 className="text-xl tracking-tight text-start text-slate-900 ">
-            {props.perfumeBrand + props.perfumeTitle}
+            Royal Mirage Brown Perfume
           </h5>
         </div>
         <div className="mt-2 mb-5 flex items-center justify-between">
           <p>
-            <span className="text-3xl font-bold text-slate-900">${props.perfumePrice}</span>
+            <span className="text-3xl font-bold text-slate-900">$40</span>
             <span className="text-sm text-slate-900 line-through">$35</span>
           </p>
           <div class="flex items-center">
@@ -77,7 +80,7 @@ export default function Card(props
               <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
             </svg>
             <span class="mr-2 ml-3 rounded bg-yellow-200 px-2.5 py-0.5 text-xs font-semibold">
-              {props.perfumeRating}
+              5.0
             </span>
           </div>
         </div>
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,18 +1,38 @@
 import React, { useEffect, useState } from "react";
 import Card from "../components/Card";
+import axios, * as others from "axios";
 
 export default function Home() {
-  const [perfumeList, usePerfumeList] = useState("");
-
+  const perfumeAPI = "https://dummyjson.com/products/category/fragrances";
+  const [isLoading, setLoading] = useState(true);
+  const [perfumeList, setPerfumeList] = useState({});
+  useEffect(() => {
+    axios
+      .get(perfumeAPI)
+      .then((res) => {
+        setPerfumeList(Object.values(res.data.products));
+        setLoading(false);
+        console.log(perfumeList[0].title)
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  useEffect(() => {
+    console.log(perfumeList);
+    console.log("Break");
+  }, [perfumeList]);
+  if (isLoading) {
+    return <div className="App">Loading...</div>;
+  }
   return (
     <>
       <div className="grid ml-16 lg:grid-cols-3 md:grid-cols-2 gap-3 ">
+        {perfumeList.map((item,i) => {return <Card key={item.id} perfumeId={item.id} perfumeBrand= {item.brand} perfumeTitle={item.title} perfumeDiscount={item.discountPercentage} perfumeRating={item.rating} perfumePrice={item.price} perfumeImg={item.images[0]} />})}
+        {/* <Card />
         <Card />
         <Card />
         <Card />
         <Card />
-        <Card />
-        <Card />
+        <Card /> */}
       </div>
     </>
   );
